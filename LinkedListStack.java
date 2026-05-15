c060128efb3c8d61df1dacad315932313202abdf
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 17:57:27 -0700

    Create LinkedListStack class with method stubs
    Initial implementation of LinkedListStack class with method stubs.

---
+++ LinkedListStack.java
@@ -0,0 +1,60 @@
+// import java.util.EmptyStackException;
+// import java.util.LinkedList;
+
+/**
+ * LinkedListStack: Stores data using the abstract data type of a stack, utilizing the two main methods for
+ * data manipulation push() and pop().
+ *
+ * @param <E> data type of elements to be stored in the linkedList
+ *
+
+ */
+
+    //we want to create a data structure that is a stack which is an illusion that acts like a stack(Last in first out LIFO)
+    //we want to use a LinkedList to store the data)
+
+    //data fields
+
+
+    /**
+     * Default Constructor, initializes an empty LinkedList.
+     */
+
+    /**
+     * Returns true if the stack is empty; otherwise, returns false
+     *
+     * @return true if empty, false otherwise
+     */
+
+
+
+    /**
+     * Returns the object at the top of the stack without removing it
+     *
+     * @return reference (shallow copy) of object at top of stack
+     */
+
+
+
+    /**
+     * Returns the object at the top of the stack and removes it
+     *
+     * @return reference of removed object from top of stack
+     */
+
+
+
+    /**
+     * Pushes an item onto the top of the stack and returns the item pushed.
+     *
+     * @param obj object to push onto top of stack
+     * @return item that was pushed
+     */
+
+
+
+    /**
+     * Returns the number of elements in the stack.
+     *
+     * @return number of elements in the stack
+     */
